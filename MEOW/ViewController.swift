6ecca9d60e574d1460da52741bd787d9ccb4425b
Ariana Fontenot <fontenot_33@aol.com>
2016-01-07 17:02:58 -0500

    centered words

---
+++ MEOW/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MEOW
+//
+//  Created by Ariana Fontenot on 1/7/16.
+//  Copyright © 2016 Fontebot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
